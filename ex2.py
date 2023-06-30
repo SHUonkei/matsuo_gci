1610749fd446a3485fe58d8ead074d97d2ad90f1
unknown <koyamashu3@keio.jp>
2023-06-30 15:04:54 +0900

    add 03

--- ex2.py
+++ ex2.py
@@ -16,15 +16,84 @@ print('インデックス:', series_i.index)
 
 
 
+print("next section -------------")
+data = {
+        'ID':['100','101','102','103','104'],
+        'City':['Tokyo','Osaka','Kyoto','Hokkaido','Tokyo'],
+        'Birth_year':[1990,1989,1992,1997,1982],
+        'Name':['Hiroshi','Akiko','Yuki','Satoru','Steve']
+        }
+#data は　dict
 
-data = {'ID':['100','101','102','103','104'],
-            'City':['Tokyo','Osaka','Kyoto','Hokkaido','Tokyo'],
-            'Birth_year':[1990,1989,1992,1997,1982],
-            'Name':['Hiroshi','Akiko','Yuki','Satoru','Steve']}
 
 df = DataFrame(data)
 
 print(df)
-print(df.head())
 
+print("section")
+print(df.head())
+print("section")
 print(df.iloc[0:4, [0,2]])
+
+print(df.iloc[0:4,[0,1]])
+print(DataFrame({10:[2],20:[3],30:[5]}))
+#{key:velue(= list)}
+#っだったらddataframeに入るのか
+
+
+
+# webからデータを取得したり、zipファイルを扱うためのライブラリ
+import requests, zipfile
+from io import StringIO
+import io
+
+# データがあるurlの指定
+url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00320/student.zip'
+
+# # データをurlから取得する
+# r = requests.get(url, stream=True)
+
+# # zipfileを読み込み展開する
+# z = zipfile.ZipFile(io.BytesIO(r.content))
+# z.extractall()
+
+student_data_math = pd.read_csv('student-mat.csv')
+print(student_data_math.head())
+
+# データの読み込み
+# 区切りに";"がついているので注意
+student_data_math = pd.read_csv('student-mat.csv', sep=';')
+student_data_math.head()
+print(student_data_math)
+
+#print(?pd.read_csv)
+
+# すべてのカラムの情報等チェック
+print(student_data_math.info())
+
+# 基本統計量をチェック
+print(student_data_math.describe())
+
+# 転置
+df.T
+print(df)
+print(df.T)
+
+# 列名の指定（1つの場合）
+print(df['Birth_year'])
+print(df.Birth_year)
+#jsみたいだな
+print(df.iloc[1])
+# 列名の指定(複数の場合)
+print(df[['ID', 'Birth_year']])
+
+#numpyのndarrayっぽい
+
+
+# 新しい列の作成
+df['Score'] = np.arange(5)*10
+print(df)
+
+
+# 行名の指定
+print(df[0:3])
